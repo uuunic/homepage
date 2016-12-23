90a566fd26894fda99d35f84020661e247a47516
uuunic <fengboang@gmail.com>
2016-12-23 14:35:09 +0800

    first commit

---
+++ homepage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+#coding=utf-8
+from django.shortcuts import render
+from homepage.models import BlogsPost
+from django.shortcuts import render_to_response
+
+# Create your views here.
+def index(request):
+    blog_list = BlogsPost.objects.all()
+    return render_to_response('index.html', {'blog_list' : blog_list})
+# Create your views here.
